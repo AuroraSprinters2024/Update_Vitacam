d9242a10254f0b5fa0b98c75827507a50a075343
unknown <shanika010712@gmail.com>
2024-10-05 20:20:40 +0530

    Home,with slideshow

--- vitacam/App.tsx
+++ vitacam/App.tsx
@@ -3,15 +3,18 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import SignUpPage from './screens/SignUp';  
 import SignInPage from './screens/SignIn';
+import Home from './screens/Home';
+
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Register" screenOptions={{headerShown: false}}>
+      <Stack.Navigator initialRouteName="Home" screenOptions={{headerShown: false}}>
         <Stack.Screen name="Register" component={SignUpPage} />
         <Stack.Screen name="Login" component={SignInPage} />
+        <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
   );
