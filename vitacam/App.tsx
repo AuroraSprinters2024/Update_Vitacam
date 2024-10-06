15f78049ec3f2b080c5995ddbaa0fa40bdd3f52c
praneeth724 <tmkpraneeth@std.appsc.sab.ac.lk>
2024-10-06 11:26:14 +0530

    Add Profile Screen

--- vitacam/App.tsx
+++ vitacam/App.tsx
@@ -1,20 +1,24 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import SignUpPage from './screens/SignUp';  
-import SignInPage from './screens/SignIn';
-import Home from './screens/Home';
-
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import SignUpPage from "./screens/SignUp";
+import SignInPage from "./screens/SignIn";
+import Home from "./screens/Home";
+import Profile from "./screens/Profile.js";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home" screenOptions={{headerShown: false}}>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{ headerShown: false }}
+      >
         <Stack.Screen name="Register" component={SignUpPage} />
         <Stack.Screen name="Login" component={SignInPage} />
         <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Profile" component={Profile} />
       </Stack.Navigator>
     </NavigationContainer>
   );
